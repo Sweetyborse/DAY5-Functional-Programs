fae9ef8d60cce462b07ab8a7cd9a5ae27593b5f6
Sweetyborse <switiborase6@gmail.com>
2022-05-06 11:50:37 +0530

    ADDING:Welcome The Functional Programs

---
+++ setMin.java
@@ -0,0 +1,20 @@
+public void setMin(int min)
+{
+   public static void main(String args[])
+{
+        if(min<=60) {
+            this.min = min;
+        }else{
+            System.out.println("Invalid input");
+        }
+    }
+
+    public void setHours(int hours){
+        this.hours=hours;
+    }
+
+    public int getHours(){
+        return this.hours;
+    }
+    
+}
